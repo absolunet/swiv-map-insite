48402cd3ffbda6f23077a44a2df7f60e815c8590
Thierry Parent <tparent@absolunet.com>
2018-07-11 11:13:25 -0400

    Use defined list only

--- src/gee/pipes/impression/list.js
+++ src/gee/pipes/impression/list.js
@@ -1,10 +1,3 @@
 module.exports = (productImpressionDataModel, productDto, context) => {
-	const lists = {
-		search: 'Search Results',
-		list: 'List Page',
-		detail: 'Detail Page'
-	};
-
-	productImpressionDataModel.list = typeof context.list !== 'undefined' ? context.list
-		: (context.properties ? context.properties.list : null) || (context.products ? lists[context.originalQuery ? 'search' : 'list'] : lists.detail);
+	productImpressionDataModel.list = context.list || (context.properties ? context.properties.list : null);
 };
